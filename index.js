7bd5d54f70799351493427e22d6f7ad0800b240b
Sam J Jimenez <sam.jimenez.27@hotmail.com>
2018-08-28 03:18:15 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,52 @@
+var string = "hello!"
+
+
+var uppercase = "string"
+ 
+uppercase.toUpperCase() === uppercase // true
+ 
+var lowercase = 'string'
+ 
+lowercase.toLowerCase() === lowercase // true
+ 
+var mixedCase = 'Hi there!'
+ 
+mixedCase.toLowerCase() === mixedCase // false
+ 
+mixedCase.toUpperCase() === mixedCase // false
+
+
+
+
+
+
+
+
+
+function shout(string) {
+    return string.toUpperCase() 
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+
+
+function sayHiToGrandma(string) {
+  if (string === lowercase ) {
+    string = "I can't hear you!";
+  }
+  else if (string === uppercase) {
+    string = "YES INDEED!";
+  }
+  else if (string === "I love you, Grandma") {
+     string = "I love you, too." ;
+  }
+  return string
+}
